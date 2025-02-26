61e5b9601029ef3a73d663a4f328dc29472cfdbb
nahinMSM <nahin_msm@hotmail.com>
2025-02-25 21:58:12 -0300

    Alterando configuração do server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const app = require("./server/index");
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL.includes("localhost") ? false : { rejectUnauthorized: false },
+  max: 5, // Define o número máximo de conexões simultâneas
+  idleTimeoutMillis: 30000, // Tempo máximo antes de encerrar conexões inativas
+  connectionTimeoutMillis: 2000, // Tempo máximo de espera para uma conexão
 });
